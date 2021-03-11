f15707d8d711d481103a10af08c5a47c546c2bae
Ringo Hoffmann <riho@live.de>
2021-03-11 13:45:44 +0100

    fix cors preflight response

--- internal/wsutil/wsutil.go
+++ internal/wsutil/wsutil.go
@@ -1,6 +1,10 @@
 package wsutil
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ErrorResponse struct {
 	Code  int    `json:"code"`
@@ -17,5 +21,10 @@ func AddCorsHeader(publicAddr string) func(*gin.Context) {
 		ctx.Header("Access-Control-Allow-Headers", "authorization, content-type, set-cookie, cookie, server")
 		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS")
 		ctx.Header("Access-Control-Allow-Credentials", "true")
+
+		if strings.ToLower(ctx.Request.Method) == "options" {
+			ctx.Status(200)
+			ctx.Abort()
+		}
 	}
 }
