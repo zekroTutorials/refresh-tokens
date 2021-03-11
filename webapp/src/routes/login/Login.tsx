0a9c82baf8ffa9c925e79745feda6fdca7078f5f
Ringo Hoffmann <riho@live.de>
2021-03-11 14:55:44 +0100

    fix login page style

--- webapp/src/routes/login/Login.tsx
+++ webapp/src/routes/login/Login.tsx
@@ -12,14 +12,15 @@ export class LoginRoute extends Component<RouteComponentProps> {
 
   render() {
     return (
-      <div>
-        <div className="login-container">
+      <div className="login-container">
+        <div>
           <input
             placeholder="Username"
             value={this.state.username}
             onChange={(e) => this.setState({ username: e.target.value })}
           ></input>
           <input
+            type="password"
             placeholder="Password"
             value={this.state.password}
             onChange={(e) => this.setState({ password: e.target.value })}
